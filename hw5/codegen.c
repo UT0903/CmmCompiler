6dbce18474f0b90f0a5a94440aa808a45ac46997
waffle <b07902011@ntu.edu.tw>
2020-12-28 19:45:30 +0800

    while

--- hw5/codegen.c
+++ hw5/codegen.c
@@ -508,7 +508,7 @@ void genIfStmt(AST_NODE* ifNode){
 	if(ELSE->nodeType == NUL_NODE){
 		genNode(test);
 		int L1 = L_ptr++, L2 = L_ptr++;
-		fprintf(fp, "\tbeq %s x0 ,L%d\n", getRegName(test), L1);
+		fprintf(fp, "\tbne %s, x0, ,L%d\n", getRegName(test), L1);
 		fprintf(fp, "\tj .L%d\n", L2);
 		freeReg(test->place, test->dataType);
 		fprintf(fp, ".L%d\n", L1);
@@ -518,7 +518,7 @@ void genIfStmt(AST_NODE* ifNode){
 	else{
 		genNode(test);
 		int L1 = L_ptr++, L2 = L_ptr++, L3 = L_ptr++;
-		fprintf(fp, "\tbeq %s x0 ,L%d\n", getRegName(test), L1);
+		fprintf(fp, "\tbne %s, x0, L%d\n", getRegName(test), L1);
 		fprintf(fp, "\tj .L%d\n", L2);
 		freeReg(test->place, test->dataType);
 		fprintf(fp, ".L%d\n", L1);
@@ -570,6 +570,19 @@ void genFunctionCall(AST_NODE* functionCallNode){
 }
 
 void genWhileStmt(AST_NODE* whileNode){
+	AST_NODE *test = whileNode->child;
+	AST_NODE *stmt = test->rightSibling;
+	int L = L_ptr++, L2 = L_ptr++, L3 = L_ptr++;
+	fprintf(fp, "\tj L%d\n", L);
+	fprintf(fp, ".L%d\n", L);
+	genNode(test);
+	fprintf(fp, "\tbne, %s, x0, L%d\n", getRegName(test), L2);
+	freeReg(test->place, test->dataType);
+	fprintf(fp, "\tj L%d\n", L3);
+	fprintf(fp, ".L%d\n", L2);
+	genStmt(stmt);
+	fprintf(fp, "\tj L%d\n", L);
+	fprintf(fp, ".L%d\n", L3);
 	return;
 }
 
