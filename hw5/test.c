c4ed9fd27aa3445f3da2ce840fe837de8125f2d4
ut <b07902048@tu.edu.tw>
2020-12-28 19:50:17 +0800

    pull

--- hw5/test.c
+++ hw5/test.c
@@ -1,3 +1,13 @@
-int MAIN(){
-    int a[10];
+int main(){
+    int a1, a2, a3, b = 4, c = 5, d = 6, e = 7, f = 1;
+    a[0] = 1+5 / 6 + 123 - (-23) + 34 * 5 + -e - c * b /f;
+    a[1] = f - a[0] + e - b / c * d - -f * e / 3 / 4 - c;
+    a[2] = a[1] * a[0] / a[1] * 3 + 5 - d + e;
+    write(a[0]);
+    write("\n");
+    write(a[1]);
+    write("\n");
+    write(a[2]);
+    write("\n");
+    
 }
