793d522ce0405dd729d51df7c4850ba2c2e08e6e
ut <b07902048@tu.edu.tw>
2020-12-30 15:55:57 +0800

    output

--- hw5/my.sh
+++ hw5/my.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 make
-./parser $1
+input=$1
+./parser $input
 riscv64-unknown-linux-gnu-gcc -O0 -static main.S
-qemu-riscv64 a.out
+qemu-riscv64 a.out > temp.output
+diff temp.output ${input/".c"/".output"}
+rm temp.output
